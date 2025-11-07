54bf55a099e00b4c306573c498d43dbbb9f00ca9
Patricia <pkwlo1992@gmail.com>
2025-11-06 20:56:47 -0800

    make change

--- app/components/main/Main.tsx
+++ app/components/main/Main.tsx
@@ -7,7 +7,7 @@ const Main = () => {
   return (
     <section className='home container max-w-5xl' id="home">
       <div className='intro'>
-        <h1 className='text-2xl'>Patricia Lo</h1>
+        <h1 className='text-2xl'>Patricia Lo (A00959925)</h1>
         <p>BCIT Computer Systems Technology New Grad | Junior Software Developer | Freelance Web Developer</p>
         <br />
         {/* <a href='#contact' className='btn'>Contact Me</a>
